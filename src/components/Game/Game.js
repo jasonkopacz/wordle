4920ff6c4d8365275535cc0090c3dc2de61e704f
Jason <jason@Arielles-MacBook-Pro.local>
2023-11-23 20:26:21 -0500

    added keyboard coloring

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,17 +5,21 @@ import { WORDS } from '../../data';
 import GuessInput from '../GuessInput/GuessInput';
 import PreviousGuesses from '../PreviousGuesses/PreviousGuesses';
 import Keyboard from '../Keyboard/Keyboard';
+import { checkGuess } from '../../game-helpers';
 // Pick a random word on every pageload.
 // To make debugging easier, we'll log the solution in the console.
 
 function Game() {
   const [answer, setAnswer] = React.useState(sample(WORDS));
-  console.info({ answer });
   const [guesses, setGuesses] = React.useState([]);
+  const validatedGuesses = guesses.map(guess => (
+    checkGuess(guess, answer)
+  ))
+  console.info({ answer });
   return <>
     <PreviousGuesses guesses={guesses} answer={answer} />
     <GuessInput guesses={guesses} setGuesses={setGuesses} answer={answer} setAnswer={setAnswer}/>
-    <Keyboard />
+    <Keyboard validatedGuesses={validatedGuesses} />
   </>
 }
 
--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -2,24 +2,37 @@ import React from 'react';
 import { range } from '../../utils';
 import { TOP_ROW, MIDDLE_ROW, BOTTOM_ROW } from '../../constants';
 
-function Keyboard() {
+function getStatusByLetter(validatedGuesses) {
+  const statusObj = {};
+
+  validatedGuesses.forEach((guess) => {
+    guess.forEach(({ letter, status }) => {
+      statusObj[letter] = status;
+    });
+  });
+
+  return statusObj;
+}
+
+function Keyboard({ validatedGuesses }) {
+  let statuses = getStatusByLetter(validatedGuesses)
   return(
   <div className='keyboard-wrapper'>
     <div className='keyboard-row top'>
       {
         range(10).map(i => (
-          <p className="keyboard-key" key={i}>{TOP_ROW[i]}</p>
+          <p className={`keyboard-key ${statuses[TOP_ROW[i]] || ''}`} key={i}>{TOP_ROW[i]}</p>
           ))}
     </div>
     <div className='keyboard-row middle'>
       {
         range(9).map(i => (
-          <p className="keyboard-key" key={i}>{MIDDLE_ROW[i]}</p>
+          <p className={`keyboard-key ${statuses[MIDDLE_ROW[i]] || ''}`} key={i}>{MIDDLE_ROW[i]}</p>
           ))}
     </div>
     <div className='keyboard-row bottom'>
       {range(7).map(i => (
-        <p className="keyboard-key" key={i}>{BOTTOM_ROW[i]}</p>
+        <p className={`keyboard-key ${statuses[BOTTOM_ROW[i]] || ''}`} key={i}>{BOTTOM_ROW[i]}</p>
         ))}
     </div>
   </div>
